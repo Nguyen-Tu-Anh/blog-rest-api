a4ce326a8d54ea960f25e94ea0be31b3a9a366ca
ntanh27 <ntanh27@cmcglobal.vn>
2024-04-11 10:04:52 +0700

    first init

---
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/exception/ResourceNotFoundException.java
@@ -0,0 +1,23 @@
+package com.springboot.blog.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+    @Getter
+    private String resourceName;
+    @Getter
+    private String fieldName;
+    @Getter
+    private String fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
+        super(String.format("%s not found with %s: '%s'", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+}
