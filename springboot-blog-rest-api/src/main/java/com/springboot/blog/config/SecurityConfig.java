4eabae0a0d9b5b3b57944667fee4995e5363dca6
ntanh27 <ntanh27@cmcglobal.vn>
2024-04-22 17:17:32 +0700

    regiter rest api

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.springboot.blog.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -17,9 +20,19 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+    private UserDetailsService userDetailsService;
+    @Autowired
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -27,22 +40,23 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorize) ->
 //                        authorize.anyRequest().authenticated()
                         authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                                .requestMatchers("/api/auth/**").permitAll()
                                 .anyRequest().authenticated()
                 ).httpBasic(Customizer.withDefaults());
         return http.build();
     }
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails root = User.builder()
-                .username("root")
-                .password(passwordEncoder().encode("qazsdfdw123"))
-                .roles("ADMIN")
-                .build();
-        UserDetails user1 = User.builder()
-                .username("tuanh")
-                .password(passwordEncoder().encode("qazsdfdw123"))
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(root, user1);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails root = User.builder()
+//                .username("root")
+//                .password(passwordEncoder().encode("qazsdfdw123"))
+//                .roles("ADMIN")
+//                .build();
+//        UserDetails user1 = User.builder()
+//                .username("tuanh")
+//                .password(passwordEncoder().encode("qazsdfdw123"))
+//                .roles("USER")
+//                .build();
+//        return new InMemoryUserDetailsManager(root, user1);
+//    }
 }
